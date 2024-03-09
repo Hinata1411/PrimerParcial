1209b0bb0201e4064a2a6f2930a1a3d4906685ae
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-03-09 11:52:39 -0600

    Busqueda binaria

--- src/BusquedaBinaria.java
+++ src/BusquedaBinaria.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class BusquedaBinaria {
+public class BusquedaBinaria {
+
+    int BusquedaBin(int miarray[], int datoabuscar) {
+
+        int inicio = 0;
+
+        int totaldeDatos = miarray.length - 1;
+
+        //Inicio menor o igual al total de datos
+        while (inicio <= totaldeDatos) {
+
+            //Lo divide en 2
+            int m = inicio + (totaldeDatos - inicio) / 2;
+
+            //Si es igual
+            if (miarray[m] == datoabuscar) {
+                return m;
+            } //Si es menor
+            if (miarray[m] < datoabuscar) {
+                inicio = m + 1;
+            } else { //Si es mayor
+                totaldeDatos = m - 1;
+            }
+        } return -1; //Si no está o no existe
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("        BÚSQUEDA BINARIA        ");
+        int miarray[] = {1, 2, 3, 4, 10, 6, 5, 8, 9, 7};
+        BusquedaBinaria buscarBin = new BusquedaBinaria(); //Instancia
+        System.out.println("El arreglo original es: " + Arrays.toString(miarray)); //Arreglo original en consola
+        Arrays.sort(miarray); //Arreglo ordenado
+        System.out.println("Arreglo ordenado: " + Arrays.toString(miarray)); //Arreglo ordenado en consola
+        System.out.println("Ingrese el número a buscar en el arreglo: ");
+        Scanner sr = new Scanner(System.in);
+        int buscar = sr.nextInt();
+        int resultadoBin = buscarBin.BusquedaBin(miarray, buscar);
+        if(resultadoBin == -1)
+            System.out.println("El número ingresado no se encuentra dentro de la lista de datos");
+        else
+            System.out.println("El número a buscar por búsqueda Binaria esta en el indice " + resultadoBin);
+
     }
+
 }
